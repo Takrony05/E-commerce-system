cec8120b491f65c9ead494cd0e70cfe470e09ff1
Takrony05 <abdelrahmantakrony@gmail.com>
2025-12-14 22:22:30 +0200

    Refactor database connection handling and migrate path_helper functions to utils

--- utlis/path_helper.py
+++ utlis/path_helper.py
@@ -13,6 +13,12 @@ def get_schema_path(db_filename="schema.sql"):
     schema_path = os.path.join(project_root, db_filename)
     return schema_path
 
+def get_image_path(image_name):
+    utils_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.normpath(os.path.join(utils_dir, ".."))
+    image_path = os.path.join(project_root, "gui", "assets", image_name)
+    return image_path
+
 def fetch_products():
     conn = sqlite3.connect(get_db_path())
     conn.row_factory = sqlite3.Row
