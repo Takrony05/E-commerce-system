31e4745ec8d1df097f4ec2c8399b369dab37e85e
Takrony05 <abdelrahmantakrony@gmail.com>
2025-12-14 18:37:56 +0200

    Refactor database connection to use schema path and add products_adding.py file

--- database/dp.py
+++ database/dp.py
@@ -1,11 +1,11 @@
 import sqlite3
-from database.path_helper import get_db_path
+from database.path_helper import get_schema_path
 
 def initialize_database():
     conn = sqlite3.connect("ecommerce.db")
     cursor = conn.cursor()
 
-    with open(get_db_path(), "r") as file:
+    with open(get_schema_path(), "r") as file:
         schema_sql = file.read()
 
     cursor.executescript(schema_sql)
--- database/path_helper.py
+++ database/path_helper.py
@@ -5,3 +5,9 @@ def get_db_path(db_filename="ecommerce.db"):
     project_root = os.path.normpath(os.path.join(utils_dir, ".."))
     db_path = os.path.join(project_root, db_filename)
     return db_path
+
+def get_schema_path(db_filename="schema.sql"):
+    utils_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.normpath(os.path.join(utils_dir, ".."))
+    schema_path = os.path.join(project_root, db_filename)
+    return schema_path
